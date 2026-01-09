1b4e09de4c4572bda7fbd03d591fc281f584d2cd
neel-learn <neelratanpatel78@gmail.com>
2026-01-09 13:04:13 +0530

    Update server.js
    v6

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const sgMail = require("@sendgrid/mail");
 const mongoose = require("mongoose");
-const nodemailer = require("nodemailer");
+
 const cors = require("cors");
 require("dotenv").config();
 
